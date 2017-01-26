8a2d4f8e04a944d68335196a70101a2eee08d4b5
Terry Bayne <baynet@saic.com>
2017-01-26 10:10:40 -0600

    More styling - the about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,24 @@ var React = require('react');
 var About = (props) => {
     return (
         <div>
-            <h3>About</h3>
-            <p>Sample React Weather Fetching Application</p>
+            <h1 className="text-center">About</h1>
+
+            <p>Sample React Weather Fetching Application. Build for the the The Complete
+                React Web Developer Course.</p>
+            <p>
+                Here are some of the tools used:
+            </p>
+            <ul>
+                <li>
+                    <a href="https://facebook.github.io/react">React</a>
+                    - The javascript framework uses.
+                </li>
+                <li>
+                    <a href="http://openweathermap.org">Open Weather Map</a>
+                    - Used to provide weather data.
+                </li>
+
+            </ul>
         </div>
     );
 }
